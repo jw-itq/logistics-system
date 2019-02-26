58f32b118aa754a279d0daddb8f49cea8190f704
shiwanming <1633138551@qq.com>
2019-02-26 22:19:15 +0800

    The first empty commit

---
+++ logistics-backdemo/src/test/java/com/logistics/demo/logisticsbackdemo/LogisticsBackdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.logistics.demo.logisticsbackdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogisticsBackdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
